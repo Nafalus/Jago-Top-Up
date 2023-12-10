62e0f4868850e74d7aca69975d13548aad6e266d
Nafalus <naufalfaadihilah05@gmail.com>
2023-12-10 23:04:29 +0700

    Lanjut Mene wes ngantuk

--- Code/Model/ModelGames.java
+++ Code/Model/ModelGames.java
@@ -21,7 +21,7 @@ public class ModelGames {
 
 
     // Done
-    public void addGame(){
+    public void addGame(NodeGames paramGames){
         Scanner input = new Scanner(System.in);
         ModelJSONGames modelJSONGames = new ModelJSONGames();
         ArrayList<NodeGames> listGames = modelJSONGames.readFromJSON();
@@ -30,55 +30,19 @@ public class ModelGames {
             listGames = new ArrayList<>();
         }
 
-        NodeGames newGame = new NodeGames(null, null);
-        System.out.println("Masukkan Nama Game Baru : ");
-        newGame.nameGame = input.nextLine();
-        System.out.println("Masukkan Currency Game Baru : ");
-        newGame.currencyName = input.nextLine();
-
-        // Tambah item & harga
-        boolean tambahItem = true;
-        while (tambahItem) {
-            System.out.println("Masukkan Nama Item : ");
-            String itemName = input.nextLine();
-            double itemPrice = 0;
-
-            boolean validInput = false;
-            while (!validInput) {
-                System.out.println("Masukkan Harga Item : ");
-                String inputPrice = input.nextLine();
-            
-                if (inputPrice.matches("\\d+(\\.\\d+)?")) {
-                    itemPrice = Double.parseDouble(inputPrice);
-                    validInput = true;
-                } else {
-                    System.out.println("Harga harus berupa angka. Silakan coba lagi.");
-                }
-            }
-            
-
-            // Tambah item game
-            newGame.addItem(itemName, itemPrice);
-
-            System.out.println("Tambah item lagi? (y/n): ");
-            String tambahLagi = input.nextLine();
-            tambahItem = tambahLagi.equalsIgnoreCase("y");
-        }
-
-        
-        listGames.add(newGame);
+        listGames.add(paramGames);
         
         // Cetak Data
-        System.out.println("LIST ITEM DAN HARGA");
-        for (NodeGames game : listGames) {
-            System.out.println("Game : " + game.nameGame);
-            System.out.println("Currency : " + game.currencyName);
-            System.out.println("Items:");
-            for (NodeGames.Item item : game.getItems()) {
-                System.out.println("  - " + item.itemName + ": Rp" + item.itemPrice);
-            }
-            System.out.println("-----------------------");
-        }
+        // System.out.println("LIST ITEM DAN HARGA");
+        // for (NodeGames game : listGames) {
+        //     System.out.println("Game : " + game.nameGame);
+        //     System.out.println("Currency : " + game.currencyName);
+        //     System.out.println("Items:");
+        //     for (NodeGames.Item item : game.getItems()) {
+        //         System.out.println("  - " + item.itemName + ": Rp" + item.itemPrice);
+        //     }
+        //     System.out.println("-----------------------");
+        // }
         
         modelJSONGames.writeFileJSON(listGames);
     }
@@ -122,47 +86,50 @@ public class ModelGames {
 
     
     // Done
-    public void searchGame(String nameGame) {
+    public NodeGames searchGame(String nameGame) {
         ArrayList<NodeGames> listGames = new ModelJSONGames().readFromJSON();
+        boolean found = false;
+        NodeGames paramOut = new NodeGames(null, null);
         if (listGames != null) {
             for (NodeGames game : listGames) {
                 if (game.getNameGame().equalsIgnoreCase(nameGame)) {
-                    System.out.println("Nama Game: " + game.getNameGame());
-                    System.out.println("Nama Currency Game: " + game.getCurrencyName());
-                    System.out.println("Items:");
-                    for (NodeGames.Item item : game.getItems()) {
-                        System.out.println("  - " + item.getItemName() + ": Rp" + item.getItemPrice());
-                    }
-                        System.out.println("--------------------");
-                    break;
-                }else{
-                    System.out.println("Game Tidak Dapat Ditemukan");
+                    // NodeGames paramOut = new NodeGames(null, null);
+                    paramOut = game;
                 }
+                // else{
+                //     System.out.println("Game Tidak Dapat Ditemukan");
+                //     return null;
+                // }
+            }
+            if (found) {
+                return null;
             }
         }
+        return paramOut;
     }
 
 
     // Done
-    public void updateItemPrice(String namegame) {
+    public void updateItemPrice(NodeGames paramGames) {
         Scanner input = new Scanner(System.in);
         ArrayList<NodeGames> listGames = new ModelJSONGames().readFromJSON();
         if (listGames != null) {
             for (NodeGames game : listGames) {
-                if (game.getNameGame().equalsIgnoreCase(namegame)) {
-                    for (NodeGames.Item item : game.getItems()) {
-                        System.out.println("Masukkan Nama item : ");
-                        String itemName = input.nextLine();
-                        if (item.itemName.equalsIgnoreCase(itemName)) {
-                            input.nextLine();
-                            System.out.println("Masukkan Harga Baru : ");
-                            double newPrice = input.nextDouble();
-                            item.setItemPrice(newPrice);
-                            break;
-                        }else{
-                            System.out.println("Item Tidak Ditemukan");
-                        }
-                    }
+                if (game.getNameGame().equalsIgnoreCase(paramGames.getNameGame())) {
+                    game = paramGames;
+                    // for (NodeGames.Item item : game.getItems()) {
+                    //     System.out.println("Masukkan Nama item : ");
+                    //     String itemName = input.nextLine();
+                    //     if (item.itemName.equalsIgnoreCase(itemName)) {
+                    //         input.nextLine();
+                    //         System.out.println("Masukkan Harga Baru : ");
+                    //         double newPrice = input.nextDouble();
+                    //         item.setItemPrice(newPrice);
+                    //         break;
+                    //     }else{
+                    //         System.out.println("Item Tidak Ditemukan");
+                    //     }
+                    // }
                     new ModelJSONGames().writeFileJSON(listGames);
                     break;
                 }
--- Code/View/MenuAdmin.java
+++ Code/View/MenuAdmin.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 import Code.Model.ModelGames;
 import Code.Model.ModelUser;
+import Code.Node.NodeGames;
 
 public class MenuAdmin {
     ModelGames modelgames = new ModelGames();
@@ -20,34 +21,101 @@ public class MenuAdmin {
         System.out.println("3. Hapus Game");
         System.out.println("4. Lihat Game");
         System.out.println("5. Lihat User");
-        System.out.println("6. Log Out");
+        System.out.println("6. Search Games");
+        System.out.println("7. Log Out");
         System.out.print("Pilih Menu : ");
         pilih = input.nextInt();
         
 
         switch (pilih) {
             case 1:
-               modelgames.addGame();
+            input.nextLine();
+                NodeGames newGame = new NodeGames(null, null);
+                System.out.println("Masukkan Nama Game Baru : ");
+                newGame.nameGame = input.nextLine();
+                System.out.println("Masukkan Currency Game Baru : ");
+                newGame.currencyName = input.nextLine();
+
+                // Tambah item & harga
+                boolean tambahItem = true;
+                while (tambahItem) {
+                    System.out.println("Masukkan Nama Item : ");
+                    String itemName = input.nextLine();
+                    double itemPrice = 0;
+
+                    boolean validInput = false;
+                    while (!validInput) {
+                        System.out.println("Masukkan Harga Item : ");
+                        String inputPrice = input.nextLine();
+                    
+                        if (inputPrice.matches("\\d+(\\.\\d+)?")) {
+                            itemPrice = Double.parseDouble(inputPrice);
+                            validInput = true;
+                        } else {
+                            System.out.println("Harga harus berupa angka. Silakan coba lagi.");
+                        }
+                    }
+                    
+
+                    // Tambah item game
+                    newGame.addItem(itemName, itemPrice);
+
+                    System.out.println("Tambah item lagi? (y/n): ");
+                    String tambahLagi = input.nextLine();
+                    tambahItem = tambahLagi.equalsIgnoreCase("y");
+                }
+                modelgames.addGame(newGame);
                 break;
+                
             case 2:
                 input.nextLine();
                 System.out.println("Masukkan Nama Game Yang ingin DiEdit : ");
                 String namegame = input.nextLine();
-                modelgames.updateItemPrice(namegame);;
+                NodeGames paraGames = modelgames.searchGame(namegame);
+                for (NodeGames.Item item : paraGames.getItems()) {
+                    System.out.println("Masukkan Nama item : ");
+                    String itemName = input.nextLine();
+                    if (item.itemName.equalsIgnoreCase(itemName)) {
+                        input.nextLine();
+                        System.out.println("Masukkan Harga Baru : ");
+                        double newPrice = input.nextDouble();
+                        item.setItemPrice(newPrice);
+                        break;
+                    }else{
+                        System.out.println("Item Tidak Ditemukan");
+                    }
+                }
+                modelgames.updateItemPrice(paraGames);
                 break;
+
             case 3:
                 input.nextLine();
                 System.out.println("Masukkan Nama Game Yang ingin Dihapus : ");
                 String nameGame = input.nextLine();
                 modelgames.DeleteGame(nameGame);
                 break;
+
             case 4:
                 modelgames.ViewAllGames();
                 break;
+
             case 5:
                 System.out.println("LIST USER");
                 modeluser.ViewAllUser();
                 break;
+
+            case 6 :
+                input.nextLine();
+                System.out.println("Masukkan Nama Game Yang ingin dicari : ");
+                String nameGameSearch = input.nextLine();
+                System.out.println("Nama Game : " + modelgames.searchGame(nameGameSearch).nameGame);
+                System.out.println("Nama Currency Game : " + modelgames.searchGame(nameGameSearch).getCurrencyName());
+                System.out.println("Daftar Item : ");
+                for (NodeGames.Item item : modelgames.searchGame(nameGameSearch).getItems()) {
+                    System.out.println("  - " + item.getItemName() + ": Rp" + item.getItemPrice());
+                }
+                break;
+
             // case 6:
             //     System.out.println("Log Out");
             //     break;
@@ -56,7 +124,7 @@ public class MenuAdmin {
                 break;
         }
         
-    }while(pilih !=6);
+    }while(pilih !=7);
 
     input.close();
 }
